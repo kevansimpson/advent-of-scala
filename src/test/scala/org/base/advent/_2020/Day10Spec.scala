7e64ab57e9cf28f1e396b3cb2875d99eec20509a
Kevan Simpson <kevasimpson@ebay.com>
2020-12-18 09:47:26 -0800

    Part 1, Day10 2020 + memory issue part 2

---
+++ src/test/scala/org/base/advent/_2020/Day10Spec.scala
@@ -0,0 +1,39 @@
+package org.base.advent._2020
+
+import org.base.advent.AdventSpec
+import org.base.advent._2020.Day10Spec.Environment
+import org.base.advent.util.LineSplitter
+import org.mockito.scalatest.IdiomaticMockito
+
+object Day10Spec extends IdiomaticMockito with LineSplitter {
+  trait Environment {
+    val day = new Day10
+    val example1: Seq[Int] = Seq(16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4)
+    val example2: Seq[Int] = Seq(28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+      8, 17, 7, 9, 4, 2, 34, 10, 3)
+  }
+}
+
+class Day10Spec extends AdventSpec {
+  "Day10" when {
+    "practicing examples" should {
+      "find product of jolt differences" in new Environment {
+        day.joltProduct(example1) shouldEqual 35
+        day.joltProduct(example2) shouldEqual 220
+      }
+      "count distinct arrangements" in new Environment {
+        day.distinctArrangements(example1) shouldEqual 8
+//        day.distinctArrangements(example2) shouldEqual 19208
+      }
+    }
+
+    "solving" should {
+      "answer part 1" in new Environment {
+        day.solvePart1 shouldEqual 1917
+      }
+      "answer part 2" in new Environment {
+//        day.solvePart2 shouldEqual 23761694
+      }
+    }
+  }
+}
