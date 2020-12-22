fa9af7f78cd0e93722ee698e0de7347ac14ec9ef
Kevan Simpson <kevasimpson@ebay.com>
2020-12-22 10:44:36 -0800

    Conway's Law support for Grid, Point

--- src/main/scala/org/base/advent/util/Grid.scala
+++ src/main/scala/org/base/advent/util/Grid.scala
@@ -2,6 +2,21 @@ package org.base.advent.util
 
 case class Grid(area: Array[Array[String]], height: Int, width: Int) {
 
+  def get(x: Int, y: Int): String = area.apply(y).apply(x)
+
+  def get(pt: Point): String = area.apply(pt.y).apply(pt.x)
+
+  def in(pt: Point): Boolean = pt.x >= 0 && pt.x < width && pt.y >= 0 && pt.y < height
+
+  def count(fxn: String => Boolean): Int = area.flatMap(_.toList).count(fxn)
+
+  def translate(fxn: CLaw => String): Grid = {
+    val grid = Array.ofDim[String](height, width)
+    for ((row, y) <- area.zipWithIndex)
+      for ((cell, x) <- row.zipWithIndex) grid.apply(y).update(x, fxn(CLaw(this, x, y, cell)))
+    new Grid(grid, height, width)
+  }
+
   def show(): Unit = {
     println(s"h: $height, w: $width")
     for (row <- area) {
@@ -9,7 +24,6 @@ case class Grid(area: Array[Array[String]], height: Int, width: Int) {
       println("")
     }
   }
-
 }
 
 object Grid {
@@ -23,4 +37,9 @@ object Grid {
 
     Grid(grid, height, width)
   }
+}
+
+// Conway's Law helper class
+case class CLaw(grid: Grid, x: Int, y: Int, cell: String) {
+  def surrounding(dist: Int = 1): Seq[Point] = Point(x, y).surrounding(dist).filter(grid.in)
 }
--- src/main/scala/org/base/advent/util/Point.scala
+++ src/main/scala/org/base/advent/util/Point.scala
@@ -1,14 +1,23 @@
 package org.base.advent.util
 
-class Point(val x: Int, val y: Int) {
-  def up(delta: Int): Point = move(0, delta)
-  def down(delta: Int): Point = move(0, -delta)
-  def left(delta: Int): Point = move(-delta, 0)
-  def right(delta: Int): Point = move(delta, 0)
+case class Point(x: Int, y: Int) {
+  def up(delta: Int = 1): Point = move(0, delta)
+  def down(delta: Int = 1): Point = move(0, -delta)
+  def left(delta: Int = 1): Point = move(-delta, 0)
+  def right(delta: Int = 1): Point = move(delta, 0)
 
-  def move(dx: Int, dy: Int): Point = new Point(x + dx, y + dy)
+  def move(dx: Int, dy: Int): Point = Point(x + dx, y + dy)
 
-  def cardinal: Seq[Point] = Seq(up(1), right(1), down(1), left(1))
+  def cardinal: Seq[Point] = Seq(up(), right(), down(), left())
+
+  def surrounding(dist: Int = 1): Seq[Point] = {
+    val (up1, right1, down1, left1): (Point, Point, Point, Point) = (up(dist), right(dist), down(dist), left(dist))
+    Seq(up1, up1.right(dist), right1, right1.down(dist), down1, down1.left(dist), left1, left1.up(dist))
+  }
 
   override def toString: String = s"[$x,$y]"
+}
+
+object Point {
+  def apply(x: Int = 0, y: Int = 0) = new Point(x, y)
 }
